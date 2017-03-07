ac99a0a9e3d5defec09089f15d3818a9b5d170de
Suresh Marru <smarru@apache.org>
2017-03-07 10:41:33 -0500

    changing links to https - testing git pull

--- partials/header.blade.php
+++ partials/header.blade.php
@@ -25,8 +25,8 @@
       </div>
     </div>  
 
-    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
     
     
     <!--[if IE 9]>
