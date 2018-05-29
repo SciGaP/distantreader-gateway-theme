bea041aea8ba074e848fd0c19e50ae872528621a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-05-29 14:08:22 -0400

    Adding Search SRA docuemntation

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -7,14 +7,20 @@
 
         <ul style="list-style-type:square">
           <li>Create a gateway account in https://www.searchsra.org/. Use 'Create account'. Provide your details and you will receive an email for account verification.</li>
-          <li>CLick the link in the email received and confirm your email. Hint: The email sometimes tend to land on 'Junk email' or 'Trash'.</li>
+          <li>Click the link in the email received and confirm your email. Hint: The email sometimes tend to land on 'Junk email' or 'Trash'.</li>
           <li>When gateway administrator grants access, you would receive a notification email.</li>
           <li>Login to the gateway, You are ready to Search thorugh the SRA!</li>
         </ul>
 
-      <h5></h5>
+      <h5>Getting Started</h5>
+          <ul style="list-style-type:square">
+            <li>When logged in you would be in your home page, User Dashboard.</li>
+            <li>You would see</li>
+            <li>As a gateway user, you could browse your old projects or experiments using 'Browse Projects' or 'Browse Experiments'.</li>
+            <li>To create new projects or experiments navigate through the menu bar on top.</li>
+            <li>What is a Project? - Project is a collection of experiments, it groups experiments. When creating an experiment, you can select which project to create with.</li>
+            <li>What is an Experiment? - Experiment is the gateway record you create in order to launch a job in a remote cluster/supercomputer/HPC.</li>
+  </ul>
     <p></p>
 
-
-    <p> Note: For production usage, we recommend using the GridChem Desktop client.</p>
 </div>
