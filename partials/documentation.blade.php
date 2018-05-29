3db90361f3db0f8341027b0d5a4a38428b04fb3f
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-05-29 13:52:33 -0400

    Adding Search SRA docuemntation

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -1,7 +1,18 @@
 <div class="container">
-  <div class="content">
-    <div class="text-center">
-      <h2 class="title top-space">Documentation page</h2>            
-    </div>
-  </div>
+  <h2 class="text-center">Training and Documentation</h2>
+    <hr class="separation-line blue-bg"/>
+
+    <h5>Search SRA Gateway User Manual</h5>
+      <h6>Gateway Access</h6>
+
+        <ul>
+          <li>Create a gateway account in https://www.searchsra.org/. Use 'Create account'. Provide your details and you will receive an email for account verification.</li>
+          <li>CLick the link in the email received and confirm your email. Hint: The email sometimes tend to land on 'Junk email' or 'Trash'.</li>
+        </ul>
+    <p> SEAGrid Gateway has evolved from initial focus in Chemistry (GridChem) encompassing broader Science and
+      Engineering Applications. In addition to the desktop client, SEAGrid provides a Web Browser based gateway access.
+      The SEAGrid gateway is in active development and we welcome you to try the pre-production alpha release and provide feedback.</p>
+
+
+    <p> Note: For production usage, we recommend using the GridChem Desktop client.</p>
 </div>
