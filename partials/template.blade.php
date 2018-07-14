01610058d16525a0f14c0781b65020567d6f56b7
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-14 13:14:23 -0400

    New Protein Search Page

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -28,7 +28,7 @@
       </p>
       <hr/>
       <!-- <button class="btn btn-primary btn-lg">Accelerate Research</button> -->
-      <p style="font-size:14px; margin-top:20px;">
+      <p style="font-size:18px; margin-top:20px;">
         We are generating sequence data at an alarming rate! The <a href="https://www.ncbi.nlm.nih.gov/sra/">Sequence Read Archive</a> currently contains ~10 petabases of sequence data (~1016 bp) and through 2016 has grown at approximately 10 TB of sequence data per day. We’re here to help you search through that data and find meaning to your own data! At searchsra.org we have identified the metagenomics data in the sequence read archive using the open source <a href="https://github.com/linsalrob/partie/">PARTIE</a>, and we provide both nucleotide and protein searches so that you can explore that data. Click on one of the buttons to learn more, or register to create an account and get started.
       </p>
       <a href="proteinsearch.php" class="button">Protein Search</a>
