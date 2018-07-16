2688c89daef8ec3ae47f6bf5d80b0b7b62492152
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 16:48:35 -0400

    Adding bibtex file for download

--- partials/citeus.php
+++ partials/citeus.php
@@ -12,14 +12,14 @@
         You can download the <a href="/assets/img/ref.tex/" download>bibtex file</a> with these citations and import that into your favorite reference manager.
         </p>
         <ul style="list-style-type:square">
-        <li style="font-size:18px" align="left">Levi, Kyle Rynge, Mats Abeysinghe, Eroma, and Edwards, Robert A. Searching the Sequence Read Archive using Jetstream and WranglerPEARC 18.</li></ul>
+        <li style="font-size:18px" align="left">Levi, Kyle Rynge, Mats Abeysinghe, Eroma, and Edwards, Robert A. Searching the Sequence Read Archive using Jetstream and WranglerPEARC 18.</li>
 
-        <li style="font-size:18px" align="left">Torres PJ, Edwards RA, McNair KA. 2017. PARTIE: a partition engine to separate metagenomic and amplicon projects in the Sequence Read Archive. Bioinformatics 33:2389–2391.</li></u>
-        <li style="font-size:18px" align="left">Towns J, Cockerill T, Dahan M, Foster I, Gaither K, Grimshaw A, Hazlewood V, Lathrop S, Lifka D, Peterson GD, Roskies R, Scott JR, Wilkins-Diehr N. 2014. XSEDE: Accelerating Scientific Discovery. Computing in Science Engineering 16:62–74.</li></u>
-        <li style="font-size:18px" align="left">Stewart CA, Cockerill TM, Foster I, Hancock D, Merchant N, Skidmore E, Stanzione D, Taylor J, Tuecke S, Turner G, Vaughn M, Gaffney NI. 2015. Jetstream: a self-provisioned, scalable science and engineering cloud environment, p. 29. In Proceedings of the 2015 XSEDE Conference: Scientific Advancements Enabled by Enhanced Cyberinfrastructure. ACM.</li></u>
-        <li style="font-size:18px" align="left">Buchfink B, Xie C, Huson DH. 2015. Fast and sensitive protein alignment using DIAMOND. Nat Methods 12:59–60.</li></u>
-        <li style="font-size:18px" align="left">Langmead B, Salzberg SL. 2012. Fast gapped-read alignment with Bowtie 2. Nat Methods 9:357–359.</li></u>
-
+        <li style="font-size:18px" align="left">Torres PJ, Edwards RA, McNair KA. 2017. PARTIE: a partition engine to separate metagenomic and amplicon projects in the Sequence Read Archive. Bioinformatics 33:2389–2391.</li>
+        <li style="font-size:18px" align="left">Towns J, Cockerill T, Dahan M, Foster I, Gaither K, Grimshaw A, Hazlewood V, Lathrop S, Lifka D, Peterson GD, Roskies R, Scott JR, Wilkins-Diehr N. 2014. XSEDE: Accelerating Scientific Discovery. Computing in Science Engineering 16:62–74.</li>
+        <li style="font-size:18px" align="left">Stewart CA, Cockerill TM, Foster I, Hancock D, Merchant N, Skidmore E, Stanzione D, Taylor J, Tuecke S, Turner G, Vaughn M, Gaffney NI. 2015. Jetstream: a self-provisioned, scalable science and engineering cloud environment, p. 29. In Proceedings of the 2015 XSEDE Conference: Scientific Advancements Enabled by Enhanced Cyberinfrastructure. ACM.</li>
+        <li style="font-size:18px" align="left">Buchfink B, Xie C, Huson DH. 2015. Fast and sensitive protein alignment using DIAMOND. Nat Methods 12:59–60.</li>
+        <li style="font-size:18px" align="left">Langmead B, Salzberg SL. 2012. Fast gapped-read alignment with Bowtie 2. Nat Methods 9:357–359.</li>
+        </ul>
         <br></br>
     </div>
   </div>
