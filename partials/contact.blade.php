298ef9fa0c941b4dd2c9289798c3a77d79221a11
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 11:30:57 -0400

    Changed the page title position

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-      <h2 class="title top-space">Contact page</h2>
+      <h2 class="text-center">Contact page</h2>
       <hr class="separation-line blue-bg"/>
     </div>
   </div>  
