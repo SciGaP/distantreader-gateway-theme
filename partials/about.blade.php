1639a8e526275998cf0aa5b3e825abb22630846a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 13:25:53 -0400

    Adding content to About page.

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -4,7 +4,7 @@
         <h2 class="text-center">About</h2>
         <hr class="separation-line blue-bg"/>
 
-        <p style="font-size:18px; margin-top:20px;">
+        <p style="font-size:18px; margin-top:20px;" align="left">
         Searching the SRA was the brainchild of <a href="https://edwards.sdsu.edu/research/">Rob Edwards</a> at <a href="https://www.sdsu.edu/">San Diego State University</a>. As a pioneer of metagenomics, Rob was frustrated that most of the DNA sequences that were publicly available were not searchable or useable, and he couldn’t find crAssphage in any of them. He started by <a href="https://edwards.sdsu.edu/research/sra/">exploring the SRA</a> to find out where that phage was hiding.
         </p>
         <br></br>
