579550ad39b96a806d48bdafa5280ddd0855d548
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 13:42:57 -0400

    Adding content the new page examples

---
+++ partials/examples.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <h2 class="text-center">Training and Documentation</h2>
+    <hr class="separation-line blue-bg"/>
+    <p style="font-size:18px; margin-top:20px;" align="left">
+    Rob has searched the SRA hundreds of times for crAssphage and this figure shows the abundance of crAssphage across 10,260 metagenomes that contain it.
+    </p>
+    <p style="font-size:18px; margin-top:20px;" align="left">
+    The genome is 97kb, and the horizontal axis shows the position in the genome.
+    The vertical axis shows metagenomes, each one is a row.
+    The darker the color, the more abundant crAssphage is in those metagenomes.
+    There are some regions of the exemplar phage that are not present in many of the metagenomes, as shown by the whiter regions, while much of the phage genome is highly conserved.
+    </p>
+    <p style="font-size:18px; margin-top:20px;" align="left">
+    The boxes at the bottom show the positions of individual genes along the genome.
+    </p>
+    <br><br>
+    <img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/metagenomenumber.png" style="width:500px;height:300px;" class="img-border">
+</div>
