ebe230aa584f3c4aec212472120d0179cde29a9c
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 16:18:34 -0400

    Adding videos to the examples page

--- partials/examples.php
+++ partials/examples.php
@@ -19,11 +19,11 @@
     In 2018 students at SDSU used the underpinnings of searchsra.org to develop independent research projects to explore metagenomes.
     Watch their videos to understand more about their amazing research stories:
     </p>
-    <br></br>&nbsp;
+    &nbsp;
     <ul style="list-style-type:square">
     <li style="font-size:18px">What Viruses are Carried by Mosquitoes in Southern California?</li></ul></p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/cTqNDb3VKzk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <br><br></br>&nbsp;
+    &nbsp;
     <ul style="list-style-type:square">
     <li style="font-size:18px">Virus PhiX174 - a Common Contaminant of Metagenomes</li></ul></p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/cn5i00ygg-w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
