788ccbf43f4a3b3285f65045d39c692db884e185
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-16 16:32:34 -0400

    Adding new page Cite Us

---
+++ partials/citeus.php
@@ -0,0 +1,26 @@
+<div class="container">
+  <div class="content">
+    <div class="text-center">
+        <h2 class="text-center">Cite Us</h2>
+        <hr class="separation-line blue-bg"/>
+
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        It is critical for bioinformaticians to have their work cited so we can continue to develop exciting tools for you.
+        Please be sure to not only cite searchsra.org, but to also cite the tools that we have used to build this amazing resource.
+        </p>
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        You can download the <a href="/images/ref.tex" download>bibtex</a> file with these citations and import that into your favorite reference manager.
+        </p>
+        <ul style="list-style-type:square">
+        <li style="font-size:18px">Levi, Kyle Rynge, Mats Abeysinghe, Eroma, and Edwards, Robert A. Searching the Sequence Read Archive using Jetstream and WranglerPEARC 18.</li></ul>
+
+        <li style="font-size:18px">Torres PJ, Edwards RA, McNair KA. 2017. PARTIE: a partition engine to separate metagenomic and amplicon projects in the Sequence Read Archive. Bioinformatics 33:2389–2391.</li><u/>
+        <li style="font-size:18px">Towns J, Cockerill T, Dahan M, Foster I, Gaither K, Grimshaw A, Hazlewood V, Lathrop S, Lifka D, Peterson GD, Roskies R, Scott JR, Wilkins-Diehr N. 2014. XSEDE: Accelerating Scientific Discovery. Computing in Science Engineering 16:62–74.</li><u/>
+        <li style="font-size:18px">Stewart CA, Cockerill TM, Foster I, Hancock D, Merchant N, Skidmore E, Stanzione D, Taylor J, Tuecke S, Turner G, Vaughn M, Gaffney NI. 2015. Jetstream: a self-provisioned, scalable science and engineering cloud environment, p. 29. In Proceedings of the 2015 XSEDE Conference: Scientific Advancements Enabled by Enhanced Cyberinfrastructure. ACM.</li><u/>
+        <li style="font-size:18px">Buchfink B, Xie C, Huson DH. 2015. Fast and sensitive protein alignment using DIAMOND. Nat Methods 12:59–60.</li><u/>
+        <li style="font-size:18px">Langmead B, Salzberg SL. 2012. Fast gapped-read alignment with Bowtie 2. Nat Methods 9:357–359.</li><u/>
+
+        <br></br>
+    </div>
+  </div>
+</div>
