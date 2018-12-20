173a9e9c95853e90be0594844730f92573a31626
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-12-20 10:05:13 -0800

    Adding Contact Us in to Search SRA

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -5,9 +5,10 @@
       <hr class="separation-line blue-bg"/>
         <p style="font-size:18px; margin-top:20px;" align="left">
         For more information about searchsra.org,
-        Please contact Prof. Rob Edwards at San Diego State University; Here is his <a href="https://edwards.sdsu.edu/research/SRA">Blog</a>.
+        Please contact Prof. Rob Edwards at San Diego State University; Here is his <a href="https://edwards.sdsu.edu/research/SRA">Blog</a>.</p>
+        <p style="font-size:18px; margin-top:20px;" align="left">
         More information on <a href="https://edwards.sdsu.edu/research/similarity-searching-the-sra/">Search SRA</a>
-                </p>
+        </p>
     </div>
   </div>  
 </div>
