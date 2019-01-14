939f0948b53b3509c8502f313ba1b27eea2a477d
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-01-14 12:43:06 -0500

    Fixed line spacing in sentences.

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -6,7 +6,7 @@
         <p style="font-size:18px; margin-top:20px;" align="left">
         For more information about searchsra.org, Please contact Prof. Rob Edwards at San Diego State University; </p>
         <p style="font-size:18px; margin-top:20px;" align="left"> Here is his <a href="https://edwards.sdsu.edu/research/SRA">Blog</a>.</p>
-        More information on <a href="https://edwards.sdsu.edu/research/similarity-searching-the-sra/">Search SRA</a>
+        <p style="font-size:18px; margin-top:20px;" align="left">More information on <a href="https://edwards.sdsu.edu/research/similarity-searching-the-sra/">Search SRA</a></p>
         </p>
     </div>
   </div>  
