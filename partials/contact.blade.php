7d3b078582776d95cb70cd529675339cb17e771a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-01-14 12:34:20 -0500

    Fixed a typo and added a line break

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -5,7 +5,8 @@
       <hr class="separation-line blue-bg"/>
         <p style="font-size:18px; margin-top:20px;" align="left">
         For more information about searchsra.org,
-        Please contact Prof. Rob Edwards at San Diego State University; Here is his <a href="https://edwards.sdsu.edu/research/SRA">Blog</a>.</p>
+        Please contact Prof. Rob Edwards at San Diego State University; </p>
+        <p Here is his <a href="https://edwards.sdsu.edu/research/SRA">Blog</a>.</p>
         <p style="font-size:18px; margin-top:20px;" align="left">
         More information on <a href="https://edwards.sdsu.edu/research/similarity-searching-the-sra/">Search SRA</a>
         </p>
