c37a0fc8bed504cb6c2e3714ff20c7e47a3201ac
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-04-12 15:26:09 -0400

    Adding About page to Distant Reader

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -5,19 +5,7 @@
         <hr class="separation-line blue-bg"/>
 
         <p style="font-size:18px; margin-top:20px;" align="left">
-        Searching the SRA was the brainchild of <a href="https://edwards.sdsu.edu/research/">Rob Edwards</a> at <a href="https://www.sdsu.edu/">San Diego State University</a>.
-        As a pioneer of metagenomics, Rob was frustrated that most of the DNA sequences that were publicly available were not searchable or useable, and he couldn’t find crAssphage in any of them.
-        He started by <a href="https://edwards.sdsu.edu/research/sra/">exploring the SRA</a> to find out where that phage was hiding.
-        </p>
-        <p style="font-size:18px; margin-top:20px;" align="left">
-        Rob and graduate student Kyle Levi teamed up with <a href="https://jetstream-cloud.org/">JetStream</a>, part of the <a href="https://www.xsede.org/">Extreme Science and Engineering Discovery Environment</a> supported by the <a href="https://nsf.gov/">NSF</a> to provide the compute resources required to search through the SRA.
-        With an initial trial allocation of compute resources, Rob was able to identify thousands of metagenomes that contain crAssphage. As soon as Rob described this approach, other people wanted to search their data against the SRA, and so Rob needed a solution that didn’t involve his email!
-        </p>
-        <p style="font-size:18px; margin-top:20px;" align="left">
-        Taking advantage of the XSEDEs Extended Collaborative Support Services (ECSS), Rob teamed up with Eroma Abeysinghe from <a href="https://scigap.org/">SciGP team</a> who deployed and configured the gateway front end and Mats Rynge who developed the back end of the search system.
-        </p>
-        <p style="font-size:18px; margin-top:20px;" align="left">
-        The computation is provided through grant number TG-MCB170036 from XSEDE. This work used the Extreme Science and Engineering Discovery Environment (XSEDE), which is supported by National Science Foundation grant number ACI-1548562.
+        Please add details about Distant Reader Application, services, etc....
         </p>
         <br></br>
     </div>
