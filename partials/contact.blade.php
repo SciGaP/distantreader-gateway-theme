4d6e11f3b2bcc92baefe20d1497454ab4c82c3a4
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-15 14:14:00 -0400

    Experimenting

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -3,8 +3,7 @@
     <div class="text-center">
       <h2 class="text-center">Contact Us</h2>
       <hr class="separation-line blue-bg"/>
-        <p style="font-size:18px; margin-top:20px;" align="left">Please Add contact details of the Gateway PI. This is for users and viewers of the gateway to contact for more informations, suggestions, etc
-        Typically, email, phone number, address.</p>
+        <p style="font-size:18px; margin-top:20px;" align="left">For more information, drop us a line Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;. </p>
     </div>
   </div>  
 </div>
