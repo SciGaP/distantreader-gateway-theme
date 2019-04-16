435c77813708607d8f8945c1f363cdbb1ffa4378
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-16 07:39:00 -0400

    Tweaking

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -3,11 +3,11 @@
     <div class="text-center">
         <h2 class="text-center">About</h2>
         <hr class="separation-line blue-bg"/>
-
+        <p style="font-size:18px; margin-top:20px;" align="left">The Distant Reader is a tool for reading.</p>
         <p style="font-size:18px; margin-top:20px;" align="left">
-        Please add details about Distant Reader Application, services, Project PI, etc....
+        More specifically, the Distant Reader is a system which locally harvests/caches content you specify. It then transforms the content into plain text, performs sets of natural language processing &amp; text mining against the text, saves the results in a number of formats, reduces the whole to a cross-platform database file, queries the database thus summarizing the collection, zips the results of the entire process into a single file, and makes the file available to you for further investigation -- "reading".
         </p>
-        <br></br>
+
     </div>
   </div>
 </div>
