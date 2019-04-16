1a74dc1f8a42a57ac7b48b7dd3168da5a6b128bc
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-16 07:53:40 -0400

    Tweaking

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -7,7 +7,9 @@
         <p style="font-size:18px; margin-top:20px;" align="left">
         More specifically, the Distant Reader is a system which locally harvests/caches content you specify. It then transforms the content into plain text, performs sets of natural language processing &amp; text mining against the text, saves the results in a number of formats, reduces the whole to a cross-platform database file, queries the database thus summarizing the collection, zips the results of the entire process into a single file, and makes the file available to you for further investigation -- "reading".
         </p>
-
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        The Distant Reader empowers you to use &amp; understand large amounts of textual information both quickly &amp; easily. It is akin to a book's table-of-contents or back-of-the-book index. It simplifies the process of identifying trends &amp; anomalies in a corpus. It then enables you further investigate those trends &amp; anomalies at scale.
+        </p>
     </div>
   </div>
 </div>
