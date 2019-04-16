df9c41aae9df75e6ed0470a00333a714c40a040f
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-16 07:31:45 -0400

    Tweaking

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -3,7 +3,6 @@
     <div class="text-center">
       <h2 class="text-center">Contact Us</h2>
       <hr class="separation-line blue-bg"/>
-        <p style="font-size:18px; margin-top:20px;" align="left">For more information, drop us a line:</p>
         <p style="font-size:18px; margin-top:20px;" align="left">
         	<blockquote>Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
         	Room 250-E, Hesburgh Libraries<br />
