b741910e20a48fa5fc789af7414e63c1502ba9c2
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-16 08:04:47 -0400

    Tweaking

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -10,6 +10,16 @@
         <p style="font-size:18px; margin-top:20px;" align="left">
         The Distant Reader empowers you to use &amp; understand large amounts of textual information both quickly &amp; easily. It is akin to a book's table-of-contents or back-of-the-book index. It simplifies the process of identifying trends &amp; anomalies in a corpus. It then enables you further investigate those trends &amp; anomalies at scale.
         </p>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		I don't know about you, but now-a-days I can find plenty of scholarly &amp; authoritative content. My problem is not one of discovery but instead one of comprehension. How do I make sense of all the content I find? The Distant Reader is intended to address this question by making observations against a corpus and providing tools for interpreting the results. It is my hope you will find the tool as useful as I do.
+		</p>
+		<p style="font-size:18px; margin-top:20px;" align="right">
+		----------</br />
+		Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
+		April 16, 2019
+		</p>
+
+
     </div>
   </div>
 </div>
