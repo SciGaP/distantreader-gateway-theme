c6c1e76b389bd996024a432b1a7eafe648c89aae
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-21 14:54:46 -0400

    Beginning documentation

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -1,27 +1,63 @@
 <div class="container">
-  <div class="content">
-    <div class="text-center">
-        <h2 class="text-center">Documentation</h2>
-        <hr class="separation-line blue-bg"/>
-     </div>
-        <p style="font-size:18px; margin-top:20px;" align="left">
-        There are four or five steps for to getting the Distant Reader to work for you:</p>
-        		<ol style="font-size:18px; margin-top:20px;" align="left">
-        <li>Articulate a research question: What do you want to know?</li>
-        <li>Identify one of three different types of input pointing to the content you want to "read". The input includes: a URL, a file of URLS, or just about any set of files</li>
-        <li>If you haven't already done so create an account</li>
-        <li>Log in</li>
-        <li>Submit your input</li>
-        <li>Wait</li>
-        <li>Download the results</li>
-        <li>Use the results the results to supplement your usual reading process</li>
-        </ol>
-        
-        		<p style="font-size:18px; margin-top:20px;" align="right">
-		Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
-		April 21, 2019
+	<div class="content">
+		<div class="text-center">
+			<h2 class="text-center">
+				Documentation
+			</h2>
+			<hr class="separation-line blue-bg" />
+		</div>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+			There are more than a few steps for to getting the Distant Reader to work for you:
 		</p>
-
-
-  </div>
+		<ol style="font-size:18px; margin-top:20px;" align="left">
+			<li>Articulate a research question: What do you want to know?</li>
+			<li>Identify one of three different types of input pointing to the content you want to "read": 1) a URL, 2) a file of URLS, or 3) just about any set of files.</li>
+			<li>If you haven't already done so create an account.</li>
+			<li>Log in.</li>
+			<li>Submit your input.</li>
+			<li>Wait.</li>
+			<li>Download the resulting Distant Reader "study carrel".</li>
+			<li>Use the "study carrel" to supplement your usual reading process.</li>
+		</ol>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		There is a lot of information there, and the process is faster the second time around. Each of the followings sections elaborates on the steps outlined above.
+		</p>
+		<h3>Articulate a research question</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+			Ask yourself, "What do I want to know?"
+		</p>
+		
+		<h3>Identify your input</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		The input can be any one of the following three things: a URL, a file of URLs, or just about any set of files on your computer.
+		</p>
+		<h3>Create an account</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		The Distant Reader process creates its "study carrel" for you, and consequently the Reader needs to who who to give its results to.
+		</p>
+		<h3>Log in</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		Authenticate yourself.
+		</p>
+		<h3>Submit your input</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		Share your input with the Distant Reader so it can do its good work.
+		</p>
+		<h3>Wait</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		Be patient. The Distant Reader is designed to handle hundreds of URLs pointing to 100's of megabytes of files. Depending on the size of your input, processing can take anywhere between two minutes and a few hours, with a "usual" wait time of ten to fifteen minutes.
+		</p>
+		<h3>Download your "study carrel"</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		The results of the Distant Reader process is a "study carrel" -- a .zip file containing your original content, various transformations of it, and varius sets of analysis.</p>
+		<h3>Read</h3>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		Use the contents of the "study carrel" to suppliment  your usual reading process.
+		</p>
+		
+				<p style="font-size:18px; margin-top:20px;" align="right">
+			Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
+			April 21, 2019 
+		</p>
+	</div>
 </div>
