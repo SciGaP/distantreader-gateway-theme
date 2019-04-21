daa5d821b090f2c115a4abe777587c17dfc5d6b7
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-04-21 14:21:40 -0400

    Beginning documentation

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -1,12 +1,27 @@
 <div class="container">
-  <h2 class="text-center">Training and Documentation</h2>
-    <hr class="separation-line blue-bg"/>
+  <div class="content">
+    <div class="text-center">
+        <h2 class="text-center">Documentation</h2>
+        <hr class="separation-line blue-bg"/>
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        There are four or five steps for to getting the Distant Reader to work for you:</p>
+        <ol>
+        <li>Articulate a research question: What do you want to know?</li>
+        <li>Identify one of three different types of input pointing to the content you want to "read". They include: a URL, a file of URLS, or a set of files</li>
+        <li>If you haven't already done so create an account</li>
+        <li>Log in</li>
+        <li>Submit your input</li>
+        <li>Wait</li>
+        <li>Download & analyze the results</li>
+        <li>Use the results to assisting in traditional reading method</li>
+        </ol>
+        
+        		<p style="font-size:18px; margin-top:20px;" align="right">
+		Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
+		April 21, 2019
+		</p>
 
-    <h4>Distant Reader Gateway User Manual</h4>
 
-    <h4>Distant Reader Gateway Sample Input Files</h4>
-
-
-	<h1>Documentation</h1>
-	
+    </div>
+  </div>
 </div>
