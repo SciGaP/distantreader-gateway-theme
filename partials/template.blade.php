bf868fad7d355cfb233e5482f72f56d9bb506c7e
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-05-13 07:53:07 -0400

    Simple editing

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-        <h2 class="text-center">The Distant Reader - A tool for "reading"</h2>
+        <h2 class="text-center">The Distant Reader - A tool for reading</h2>
         <hr class="separation-line blue-bg"/>
         <p style="font-size:18px; margin-top:20px;" align="left">
         The Distant Reader is a tool for reading.
