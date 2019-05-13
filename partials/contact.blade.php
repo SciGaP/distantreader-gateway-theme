95572ca4edb3b1827d92bb2bd2af4d04cbc4330e
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-05-13 07:44:58 -0400

    Simple editing

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -3,14 +3,15 @@
     <div class="text-center">
       <h2 class="text-center">Contact Us</h2>
       <hr class="separation-line blue-bg"/>
+        <p style="font-size:18px; margin-top:20px;" align="left">If you have any questions, then just drop us a line. We love to get email.</p>
         <p style="font-size:18px; margin-top:20px;" align="left">
-        	<blockquote>Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
+        	Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
         	Room 250-E, Hesburgh Libraries<br />
         	University of Notre Dame<br />
         	Notre Dame, IN 46556<br />
         	<br />
         	+1 (574) 631-8604
-        	</blockquote>
+        	
         </p>
     </div>
   </div>  
