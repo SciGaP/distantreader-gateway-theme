ea3fc88e19f0a0fc07aae7f012e7fd12019796d4
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-05-13 07:38:40 -0400

    Simple editing

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -4,8 +4,23 @@
         <h2 class="text-center">The Distant Reader - A tool for "reading"</h2>
         <hr class="separation-line blue-bg"/>
         <p style="font-size:18px; margin-top:20px;" align="left">
-        [INSERT HOME PAGE TEXT HERE]
+        The Distant Reader is a tool for reading.
         </p>
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        The Distant Reader empowers you to <em>use &amp; understand</em> large amounts of textual information both quickly &amp; easily. For example, the Distant Reader can consume the entire issue of a scholarly journal, the complete works of a given author, or the content found at the other end of an arbitrarily long list of URLs. Thus, the Distant Reader is akin to a book's table-of-contents or back-of-the-book index but at scale. It simplifies the process of identifying trends &amp; anomalies in a corpus, and then it enables you to further investigate those trends &amp; anomalies.
+        </p>
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        Technically speaking, the Distant Reader is a system which locally harvests/caches content you specify. It then transforms the content into plain text, performs sets of natural language processing &amp; text mining against the text, saves the results in a number of formats, reduces the whole to a cross-platform database file, queries the database thus summarizing the collection, zips the results of the entire process into a single file, and makes the file available to you for further investigation -- "reading".
+        </p>
+		<p style="font-size:18px; margin-top:20px;" align="left">
+		I don't know about you, but now-a-days I can find plenty of scholarly &amp; authoritative content. My problem is not one of discovery but instead one of comprehension. How do I make sense of all the content I find? The Distant Reader is intended to address this question by making observations against a corpus and providing tools for interpreting the results. It is my hope you will find the tool as useful as I do.
+		</p>
+		<p style="font-size:18px; margin-top:20px;" align="right">
+		Eric Lease Morgan &lt;<a href="mailto:emorgan@nd.edu">emorgan@nd.edu</a>&gt;<br />
+		April 16, 2019
+		</p>
+
+
     </div>
   </div>
 </div>
