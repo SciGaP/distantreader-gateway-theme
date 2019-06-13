3bde1bcfa84137035bc13b015e6dab30343bc4e9
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-06-13 11:23:01 -0400

    Updating home page

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -12,6 +12,53 @@
         <p style="font-size:18px; margin-top:20px;" align="left">
         Technically speaking, the Distant Reader is a system which locally harvests/caches content you specify. It then transforms the content into plain text, performs sets of natural language processing &amp; text mining against the text, saves the results in a number of formats, reduces the whole to a cross-platform database file, queries the database thus summarizing the collection, zips the results of the entire process into a single file, and makes the file available to you for further investigation -- "reading".
         </p>
+
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        Here is a list of sample corpora, their generic outputs, and links to their data sets:
+        </p>
+
+<ul>
+	<li>Cultural Analytics - <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/cultural-analytics/about.html">about page</a>, <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/cultural-analytics.zip">data set</a>
+<ul>
+	<li>318,287 words; 33 documents; 67 uncompressed MB</li>
+	<li>all articles from a journal named Cultural Analytics</li>
+</ul>
+
+	</li>
+    
+	<li>Plato - <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/plato.zip">http://dh.crc.nd.edu/sandbox/reader/hackaton/plato.zip</a>
+<ul>
+	<li>929,704 words; 24 documents; 112 uncompressed MB</li>
+	<li>the complete works of Plato</li>
+</ul>
+
+	</li>
+    
+	<li>Code4Lib Journal - <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/code4lib-journal.zip">http://dh.crc.nd.edu/sandbox/reader/hackaton/code4lib-journal.zip</a>
+<ul>
+	<li>1,234,348 words; 303 documents; 286 uncompressed MB</li>
+	<li>all articles from a journal named Code4Lib Journal</li>
+</ul>
+
+	</li>
+    
+	<li>aesthetics - <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/aesthetics.zip">http://dh.crc.nd.edu/sandbox/reader/hackaton/aesthetics.zip</a>
+<ul>
+	<li>2,296,890 words; 37 documents; 287 uncompressed MB</li>
+	<li>books classified as the philosophy of art</li>
+</ul>
+
+	</li>
+	<li>love stories - <a href="http://dh.crc.nd.edu/sandbox/reader/hackaton/love-stories.zip">http://dh.crc.nd.edu/sandbox/reader/hackaton/love-stories.zip</a>
+		<ul>
+			<li>238,374,038 words; 460 documents; 5.94 uncompressed GB</li>
+			<li>books classified as love stories</li>
+		</ul>
+	</li>
+
+</ul>
+
+
 		<p style="font-size:18px; margin-top:20px;" align="left">
 		I don't know about you, but now-a-days I can find plenty of scholarly &amp; authoritative content. My problem is not one of discovery but instead one of comprehension. <em>How do I make sense of all the content I find?</em> The Distant Reader is intended to address this question by making observations against a corpus and providing tools for interpreting the results. It is my hope you will find the tool as useful as I do.
 		</p>
